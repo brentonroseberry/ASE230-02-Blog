a857ff72b849f03e2c02208e2b5c72c1656779b8
Brenton Roseberry <brenton.roseberry@gmail.com>
2024-09-21 17:00:36 -0400

    assignment 4 done

--- detail.php
+++ detail.php
@@ -2,33 +2,29 @@
     <a href="index.php">Go Back</a>
 <?php
 $post_id=$_GET['post_id'];
-$blog_posts = [
-    ["title"=>"How to cook",
-        "content"=>"Just throw some stuff together, I don't know",
-        "author"=>"Gordon Ramsey",
-        "date"=>"8/13/2024"
-    ],
-    ["title"=>"Is there dinosaurs in Florida swamps?",
-        "content"=>"Have you seen alligators? What else would those things be.",
-        "author"=>"Florida Man",
-        "date"=>"8/30/2024"
-    ],
-    ["title"=>"Dogs?",
-        "content"=>"No, get a cat.",
-        "author"=>"Old Lady",
-        "date"=>"9/2/2024"
-    ]
-]
+$posts = file_get_contents("posts.json");
+$posts = json_decode($posts, true);
+$line = file_get_contents("visitors.csv");
+$count = explode("\n", $line);
+for ($i = 0; $i <= $post_id; $i++) {
+    if (!isset($count[$i])) {
+        $count[$i] = 0;
+    }
+}
+$count[$post_id]++;
+$fp = fopen("visitors.csv", "w");
+fputs($fp, implode("\n",$count).PHP_EOL);
+fclose($fp);
 ?>
     <head>
-        <title>Assignment3</title>
+        <title>Assignment 3 and 4</title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
-        <h2><?=$blog_posts[$post_id]["title"]?></h2>
-        <h3>By: <?=$blog_posts[$post_id]["author"]?></h3>
-        <h4>Published <?=$blog_posts[$post_id]["date"]?></h4>
-        <p><?=$blog_posts[$post_id]["content"]?></p>
+        <h2><?=$posts[$post_id]["Title"]?></h2>
+        <h3>By: <?=$posts[$post_id]["Author"]?></h3>
+        <h4>Published: <?=$posts[$post_id]["Date"]; echo "<br>"?>Views: <?=$count[$post_id]?></h4>
+        <p><?=$posts[$post_id]["Content"]?></p>
     </body>
 
 </html>
--- index.php
+++ index.php
@@ -1,36 +1,21 @@
 <!--Made by Brenton Roseberry-->
 <?php
-$blog_posts = [
-    ["title"=>"How to cook",
-        "content"=>"Just throw some stuff together, I don't know",
-        "author"=>"Gordon Ramsey",
-        "date"=>"8/13/2024"
-    ],
-    ["title"=>"Is there dinosaurs in Florida swamps?",
-        "content"=>"Have you seen alligators? What else would those things be.",
-        "author"=>"Florida Man",
-        "date"=>"8/30/2024"
-    ],
-    ["title"=>"Dogs?",
-        "content"=>"No, get a cat.",
-        "author"=>"Old Lady",
-        "date"=>"9/2/2024"
-    ]
-];
+$posts = file_get_contents("posts.json");
+$posts = json_decode($posts, true);
 function printTitles($i, $blog_post){?>
     <div>
-        <h3><a href="detail.php?post_id=<?= $i ?>"><?= $blog_post["title"]?></a></h3>
-        <h4>By: <?= $blog_post["author"]?></h4>
+        <h3><a href="detail.php?post_id=<?= $i ?>"><?= $blog_post["Title"]?></a></h3>
+        <h4>By: <?= $blog_post["Author"]?></h4>
         <br>
     </div>
 <?php } ?>
 <html>
     <head>
-        <title>Assignment3</title>
+        <title>Assignment 3 and 4</title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
         <h2>Blog Posts:</h2><hr>
-        <?php for($i=0;$i<count($blog_posts);$i++){ printTitles($i, $blog_posts[$i]); }?>
+        <?php for($i=0;$i<count($posts);$i++){ printTitles($i, $posts[$i]); }?>
     </body>
 </html>
